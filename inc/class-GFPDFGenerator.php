890b68fe5526ce79651c1f6afa33ae80863d7254
Haseeb <contact@mhaseeb.com>
2019-10-27 15:21:04 +0500

    Organized PDF Generator Class

--- inc/class-GFPDFGenerator.php
+++ inc/class-GFPDFGenerator.php
@@ -20,6 +20,10 @@ class GFPDFGenerator {
             ] );
     }
 
+    
+    /**
+     *  Generating PDF
+     */
     public function save() {
         
         $this->setHeader( $this->getSetting('header_text') );
@@ -30,10 +34,15 @@ class GFPDFGenerator {
         
         $this->setContent( $this->getSetting('pdf_content') );
         
-        
         $this->mPDF->Output( $this->filepath , \Mpdf\Output\Destination::FILE );
     }
 
+    
+    /**
+     * Prepearing Header for the PDF
+     * 
+     * @return string html code for the header
+     */
     private function setHeader( $html ) {
         $html = $this->maybeFormat( $html );
         $html = apply_filters( $this->filter_tag( 'header_html' ) , $html );
@@ -41,6 +50,11 @@ class GFPDFGenerator {
         $this->mPDF->SetHTMLHeader( $html );
     }
 
+    /**
+     * Prepearing Content for the PDF
+     * 
+     * @return string html code for the main PDF content
+     */
     private function setContent( $html ) {
 
         $html = $this->maybeFormat( $html );
@@ -49,6 +63,11 @@ class GFPDFGenerator {
         $this->mPDF->WriteHTML( $html );
     }
 
+    /**
+     * Prepearing Footer for the PDF
+     * 
+     * @return string html code for the footer
+     */
     private function setFooter( $html ) {
         $html = $this->maybeFormat( $html );
         $html = apply_filters( $this->filter_tag( 'footer_html' ) , $html );
@@ -64,6 +83,11 @@ class GFPDFGenerator {
         $this->mPDF->SetHTMLFooter( $html );
     }
 
+    /**
+     * Styles for the PDF 
+     * 
+     * @return string style code for the PDF
+     */
     private function addStyles() {
         $styles = '<style media="all">';
 
@@ -96,6 +120,11 @@ class GFPDFGenerator {
         $this->mPDF->WriteHTML( $styles );
     }
 
+    /**
+     * Font Directory for add custom fonts
+     * 
+     * @return array font directory for initialization
+     */
     private function addFontDir() {
         $defaultConfig = (new Mpdf\Config\ConfigVariables())->getDefaults();
         $fontDirs = $defaultConfig['fontDir'];
@@ -103,6 +132,11 @@ class GFPDFGenerator {
         return array_merge( $fontDirs,  [ GF_PDF_ADDON_PATH . 'fonts' ] );
     }
 
+    /**
+     * Font Data to add custom fonts
+     * 
+     * @return array font data for initialization
+     */
     private function addFontData() {
         $fonts = [
             "opensans" => [
@@ -125,6 +159,13 @@ class GFPDFGenerator {
         return $fontData + $fonts;
     }
 
+    /**
+     * PDF addon settings
+     * 
+     * @param string settings name
+     * 
+     * @return string value for the setting
+     */
     private function getSetting( $name, $default = false ) {
         if( isset( $this->form['gravityforms-to-pdf'][$name] ) )
             return $this->form['gravityforms-to-pdf'][$name];
@@ -132,10 +173,24 @@ class GFPDFGenerator {
         return $default;
     }
 
+    /**
+     * filter with prefixfor the PDF html code
+     * 
+     * @param string part of the pdf 
+     * 
+     * @return string filter hook for extension / replacement 
+     */
     private function filter_tag( $tag ) {
         return $this->filterPrefix . $tag;
     }
 
+    /**
+     * Formatting for the html code
+     * 
+     * @param string html code 
+     * 
+     * @return string html code formatted and merged tags
+     */
     private function maybeFormat( $html ) {
         $html = $this->merge_tags($html);
         return wpautop( $html );
@@ -147,6 +202,14 @@ class GFPDFGenerator {
         ];
     }
 
+    /**
+     * Merge Tags replacement 
+     * Form fields merge tags & custom merge tags for the PDF file
+     * 
+     * @param string html code with the merge tags
+     * 
+     * @return string html code with the merged values
+     */
     private function merge_tags ( $html ) {
         preg_match_all('/{(\w+)}/', $html, $matches);
 
